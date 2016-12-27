1119de70336b69976f3319356eff38cca7c95f6c
LoopWestWorld <n5r9@foxmail.com>
2016-12-27 09:44:51 +0800

    7th commit test

--- test6.py
+++ test6.py
@@ -2,7 +2,9 @@
 # @Author: LoopWestWorld
 # @Date:   2016-12-27 09:38:59
 # @Last Modified by:   LoopWestWorld
-# @Last Modified time: 2016-12-27 09:39:14
+# @Last Modified time: 2016-12-27 09:44:26
 
 
 print "6666666666"
+
+print "777777777"
