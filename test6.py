24ec364f59db7a7a8d66e3b9e4e77ea5cc569c34
LoopWestWorld <n5r9@foxmail.com>
2016-12-27 09:40:41 +0800

    6th test commit

---
+++ test6.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: LoopWestWorld
+# @Date:   2016-12-27 09:38:59
+# @Last Modified by:   LoopWestWorld
+# @Last Modified time: 2016-12-27 09:39:14
+
+
+print "6666666666"
